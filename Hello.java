37df0e6883800beb4affa18f28be17735940d0de
Harsh Vardhan Guleria <harshvardhanguleria@gmail.com>
2021-02-05 09:47:38 +0530

    Modified

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
-class Simple{
+class Hello{
     public static void main(String args[]){
         System. out. println("Hello Java, this is HVG");
-        System. out. println("My name is Harsh Vardhan Guleria");
+        System. out. println("HVG stands for Harsh Vardhan Guleria");
     }
 }
