86859a3487e1978559749042bc42c4b4e0f003f4
Harsh Vardhan Guleria <harshvardhanguleria@gmail.com>
2021-02-13 13:50:56 +0530

    Modified

---
+++ Udemy_course_basics.java
@@ -0,0 +1,8 @@
+public class Udemy_course_basics {
+    public static void main (String[] args) {
+        double pounds = 200;
+        double kilograms = pounds * 0.45359237;
+        System.out.println("200 Pounds in kilograms is " + 
+            kilograms);
+    }
+}
