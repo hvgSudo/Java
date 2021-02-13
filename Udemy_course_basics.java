c5a58636404b4d6f18262c84bc509331e36b8c19
Harsh Vardhan Guleria <harshvardhanguleria@gmail.com>
2021-02-13 23:51:54 +0530

    Modified

--- Udemy_course_basics.java
+++ Udemy_course_basics.java
@@ -1,8 +1,15 @@
 public class Udemy_course_basics {
     public static void main (String[] args) {
-        double pounds = 200;
-        double kilograms = pounds * 0.45359237;
-        System.out.println("200 Pounds in kilograms is " + 
-            kilograms);
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        char myCopyrightChar = '\u00A9';
+        System.out.println("myChar: " + myChar);
+        System.out.println("myUnicodeChar: " + myUnicodeChar);
+        System.out.println("myCpoyrihtChar: " + myCopyrightChar);
+        
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+        boolean isCustomerOverTwentyOne = true;
+        
     }
 }
