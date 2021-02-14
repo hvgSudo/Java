6d1533e0ef43228fe5260f880ea4c7d701d353c9
Harsh Vardhan Guleria <harshvardhanguleria@gmail.com>
2021-02-14 10:51:04 +0530

    Modified

--- Udemy_course_basics.java
+++ Udemy_course_basics.java
@@ -1,15 +1,16 @@
 public class Udemy_course_basics {
     public static void main (String[] args) {
-        char myChar = 'D';
-        char myUnicodeChar = '\u0044';
-        char myCopyrightChar = '\u00A9';
-        System.out.println("myChar: " + myChar);
-        System.out.println("myUnicodeChar: " + myUnicodeChar);
-        System.out.println("myCpoyrihtChar: " + myCopyrightChar);
         
-        boolean myTrueBooleanValue = true;
-        boolean myFalseBooleanValue = false;
-        boolean isCustomerOverTwentyOne = true;
-        
+        double var_1 = 20.00;
+        double var2 = 80.00;
+        double result = (var_1 + var2) * 100.00;
+        double remainder = result % 40.00;
+        boolean var4 = (remainder == 0) ? true : false;
+        System.out.println(var4);
+        System.out.println("Result: "+ result);
+        System.out.println("remainder: "+ remainder);
+        if (var4 != true) {
+            System.out.println("Got some remainder");
+        }
     }
 }
