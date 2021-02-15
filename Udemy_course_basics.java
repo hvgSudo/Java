93303215c52057c135f5400ea4f86c7d991c001b
Harsh Vardhan Guleria <harshvardhanguleria@gmail.com>
2021-02-15 09:40:55 +0530

    Modified

--- Udemy_course_basics.java
+++ Udemy_course_basics.java
@@ -1,16 +1,10 @@
 public class Udemy_course_basics {
     public static void main (String[] args) {
-        
-        double var_1 = 20.00;
-        double var2 = 80.00;
-        double result = (var_1 + var2) * 100.00;
-        double remainder = result % 40.00;
-        boolean var4 = (remainder == 0) ? true : false;
-        System.out.println(var4);
-        System.out.println("Result: "+ result);
-        System.out.println("remainder: "+ remainder);
-        if (var4 != true) {
-            System.out.println("Got some remainder");
-        }
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+        System.out.println("And this is myVariable "+
+        myVariable);
     }
 }
