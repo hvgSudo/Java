71c2ba275d94997e15c3569c0b01f3949ba5837b
Harsh Vardhan Guleria <harshvardhanguleria@gmail.com>
2021-02-16 19:30:07 +0530

    Modified

--- Fib_fact.java
+++ Fib_fact.java
@@ -16,21 +16,21 @@ public class Fib_fact {
             return (fibonacci(n - 1) + fibonacci(n - 2));
     }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         int choice, number, j = 0;
         char ch;
         do {
             System.out.println("1. Factorial");
             System.out.println("2. Fibonacci series");
             System.out.print("Enter your choice: ");
-            choice = sc.nextInt();
+            choice = in.nextInt();
             if (choice == 1) {
                 System.out.print("Enter the number to find the factorial: ");
-                number = sc.nextInt();
+                number = in.nextInt();
                 System.out.println("The factorial of " + number + " is " + factorial(number));
             } else if (choice == 2) {
                 System.out.print("Enter how many digits do you want to see from the Fibonacci series: ");
-                number = sc.nextInt();
+                number = in.nextInt();
                 System.out.println("The first " + number + " fibonacci series are");
                 for (int i = 0; i < number; i++) {
                     System.out.print(fibonacci(j) + " ");
@@ -39,8 +39,8 @@ public class Fib_fact {
             }
             System.out.println();
             System.out.print("Do you want to continue(y/n): ");
-            ch = sc.next().charAt(0);
+            ch = in.next().CharAt(0);
         }while (ch == 'y' || ch == 'Y');
-        sc.close();
+        in.close();
     }
 }
--- Udemy_course_basics.java
+++ Udemy_course_basics.java
@@ -1,10 +1,33 @@
 public class Udemy_course_basics {
     public static void main (String[] args) {
-        int myVariable = 50;
-        myVariable++;
-        myVariable--;
-        System.out.println("This is a test");
-        System.out.println("And this is myVariable "+
-        myVariable);
+        String name = "Taisha";
+        int position = calculateHighScorePosition(1500);
+        displayHighScorePosition(name, position);
+        
+        position = calculateHighScorePosition(900);
+        displayHighScorePosition(name, position);
+        
+        position = calculateHighScorePosition(400);
+        displayHighScorePosition(name, position);
+
+        position = calculateHighScorePosition(50);
+        displayHighScorePosition(name, position);
+    }
+
+    public static void displayHighScorePosition(String name, 
+        int position) {
+            System.out.println(name +" managed to get into "+
+                "position "+ position);
+    }
+
+    public static int calculateHighScorePosition(int score) {
+        if (score >= 1000) {
+            return 1;
+        } else if (score >= 500) {
+            return 2; 
+        } else if (score >= 100) {
+            return 3;
+        }
+        return 4;
     }
 }
