861be5aab465ef92e369116c2a37886c1def80e7
Harsh Vardhan Guleria <harshvardhanguleria@gmail.com>
2021-02-17 15:16:52 +0530

    Modified

--- Fib_fact.java
+++ Fib_fact.java
@@ -1,4 +1,8 @@
 import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.DataInputStream;
 
 public class Fib_fact {
 
@@ -15,32 +19,38 @@ public class Fib_fact {
         else
             return (fibonacci(n - 1) + fibonacci(n - 2));
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner in = new Scanner(System.in);
+        BufferedReader reader =new BufferedReader(
+            new InputStreamReader(System.in));
+        DataInputStream data = new DataInputStream(System.in);
         int choice, number, j = 0;
-        char ch;
+        String ch;
         do {
             System.out.println("1. Factorial");
             System.out.println("2. Fibonacci series");
             System.out.print("Enter your choice: ");
-            choice = in.nextInt();
+            choice = in.nextInt(); // using scanner
             if (choice == 1) {
                 System.out.print("Enter the number to find the factorial: ");
-                number = in.nextInt();
-                System.out.println("The factorial of " + number + " is " + factorial(number));
+                number = in.nextInt(); // using scanner
+                System.out.println("The factorial of " + number
+                    + " is " + factorial(number));
             } else if (choice == 2) {
-                System.out.print("Enter how many digits do you want to see from the Fibonacci series: ");
-                number = in.nextInt();
-                System.out.println("The first " + number + " fibonacci series are");
+                System.out.print("Enter how many digits do you"+
+                    " want to see from the Fibonacci series: ");
+                number = Integer.parseInt(data.readLine()); // using DataInputStream
+                System.out.println("The first " + number + 
+                    " fibonacci series are");
                 for (int i = 0; i < number; i++) {
                     System.out.print(fibonacci(j) + " ");
                     j = j + 1;
                 }
             }
             System.out.println();
-            System.out.print("Do you want to continue(y/n): ");
-            ch = in.next().CharAt(0);
-        }while (ch == 'y' || ch == 'Y');
+            System.out.print("Do you want to continue(yes/no): ");
+            ch = reader.readLine(); // using BufferedReader
+        }while (ch.equals("yes"));
         in.close();
     }
 }
--- testing.java
+++ testing.java
@@ -1,25 +1,137 @@
 import java.util.Scanner;
-public class testing {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int no1;
-        int no2;
-        int res;
-        char ch;
-        try {
-            do {
-                System.out.println("Enter first number: ");
-                no1 = Integer.parseInt(in.nextLine());
-                System.out.println("Enter second number: ");
-                no2 = Integer.parseInt(in.nextLine());
-                res = no1 + no2;
-                System.out.println("Result of " + no1 + " + " + no2 + " = " + res);
-                System.out.println("Do you want to continue(y/n): ");
-                ch = in.next().charAt(0);
-            }while(ch == 'y' || ch == 'Y');
-        } catch (Exception e) {
 
-        }
-        in.close();
+
+class Main {
+public static void main(String[] args)
+{
+double sqroot;
+long power;
+int a,b,c,k, ans, sum=0, count=0;
+ int p,q;
+double ans_avg;
+String data;
+int choice =1;
+Scanner s = new Scanner(System.in);
+calculator Obj1 = new calculator(); // java object creation
+while(true)
+{
+System.out.println("*MENU 1) Addition 2) Subtraction 3) Multiplication 4) Division 5) Square root 6)Power 7)Mean 8)GCD 9)EXIT ");
+choice=s.nextInt();
+switch(choice)
+    {
+case 1:
+    System.out.println("enter two nos");
+    a=s.nextInt();
+    b=s.nextInt();
+    ans=Obj1.sum(a,b);
+    System.out.println("Sum is= " + ans);
+    break;
+case 2:
+    System.out.println("enter two nos");
+    a=s.nextInt();
+    b=s.nextInt();
+    ans=Obj1.sub(a,b);
+    System.out.println("Subtraction is= " + ans);
+    break;
+case 3:
+    System.out.println("enter two nos");
+    a=s.nextInt();
+    b=s.nextInt();
+    ans=Obj1.mult(a,b);
+    System.out.println("Multiplication is=" +ans);
+    break;
+case 4:
+    System.out.println("enter two nos");
+    a=s.nextInt();
+    b=s.nextInt();  
+    ans=Obj1.div(a,b);
+    System.out.println("Division is=" +ans);
+    break;
+case 5:
+    System.out.println("Enter number:");
+    k=s.nextInt();
+    sqroot=Obj1.sqrt(k);
+    System.out.println("Square root is=" +sqroot);
+    break;
+case 6:
+    System.out.println("Enter two numbers:");
+    p=s.nextInt();
+    q=s.nextInt();
+    power=Obj1.pwr(p,q);
+    System.out.println("Power of the given number is=" +power);
+    break;
+//case 7:
+  //  ans=Obj1.mean();
+    //System.out.println("Mean of the array is=" +);
+    //break;
+case 7:
+    System.out.println("enter two nos");
+    a=s.nextInt();
+    b=s.nextInt();
+    c=Obj1.gcd(a,b);
+    System.out.println("The Greatest common divisor of the given numbers is=" +c);
+    break;
+case 8: System.exit(0);
     }
+}// while ends
+
+} // main ends
+} // main class ends
+class calculator
+{
+int sum(int a, int b ) // SUM function body
+{ 
+    return ( a+b); 
 }
+
+int sub(int a, int b ) // SUB function body
+{ 
+    return ( a-b); 
+}
+int mult(int a, int b)
+  {
+    return(a*b);
+  }
+  
+int div(int a, int b)
+  {
+    return(a/b);
+  }
+double sqrt(int k)
+{
+    double n;
+    double sqrt=k/2;
+    do
+    {
+        n=sqrt;
+        sqrt=(n+(k/n))/2;
+    }
+    while((n-sqrt)!=0);
+    return(sqrt);
+    
+}
+long pwr(int p, int q)
+{
+    long result=1;
+    while(q!=0)
+        {
+            result*=p;
+            --q;
+        }
+    return(result);
+}
+ //double mean(int n, int sum)
+//{ 
+  //  return (sum / n);
+//}
+
+int gcd(int a,int b)
+{
+ if(b==0)
+    {
+        return a;
+    }
+ else     
+    return gcd(b,a%b);
+}
+} // calculator closes
