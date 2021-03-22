9638c9da2e2337d00b03538c5ee1cef41d21ecdc
Harsh Vardhan Guleria <harshvardhanguleira@gmail.com>
2021-03-22 12:49:50 +0530

    Modified

--- IteratorUse.java
+++ IteratorUse.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Iterator;
 
 public class IteratorUse {
@@ -9,5 +10,25 @@ public class IteratorUse {
         Iterator<Character> i = list.iterator();
         while (i.hasNext()) 
             System.out.println("Element -> "+ i.next());
+    }
+
+    private static boolean addInOrder(LinkedList<String> 
+        linkedList, String newCity) {
+        ListIterator<String> stringListIterator = linkedList.listIterator();
+        while (stringListIterator.hasNext()) {
+            int comparison = stringListIterator.next().compareTo(newCity);
+            if (comparison == 0) {
+                System.out.println("City already present");
+                return false;
+            } else if (comparison > 0) {
+                stringListIterator.previous();
+                stringListIterator.add(newCity);
+                return true;
+            } else if (comparison < 0) {
+                // continue to next which has already been done
+                // by .next()
+            }
+        }
+        stringListIterator.add(newCity);
     }
 }
