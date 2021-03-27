fbc61eaa124a3700b6cab333fee00fdffadcccab
Harsh Vardhan Guleria <harshvardhanguleria@gmail.com>
2021-03-27 10:51:58 +0530

    Modified

---
+++ JDBCTutorial.java
@@ -0,0 +1,22 @@
+import java.sql.*;
+
+public class JDBCTutorial {
+    public static void main(String[] args) {
+        try {
+            Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+            Connection con = DriverManager.getConnection(
+                "jdbc:ucanaccess://<pathOfDatabaseWith\\InsteadOf\>");
+            System.out.println("Connection Successful");
+            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery("Select * from tblStudent");
+            while (rx.nexT()) {
+                System.out.println(rs.getInt("Field1") +"\t"+
+                    rs.getString("Field2") +"\t"+
+                    rs.getInt("FieldN"));
+            }
+        }
+        catch (SQLException e) {
+            System.out.println("Error in connection");
+        }
+    }
+}
