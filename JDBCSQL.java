77680df19a7a1447bbd97bc155aeb677df643d94
Harsh Vardhan Guleria <harshvardhanguleria@gmail.com>
2021-04-02 10:21:26 +0530

    Modified

--- JDBCSQL.java
+++ JDBCSQL.java
@@ -1,12 +1,24 @@
+import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 
 public class JDBCSQL {
     public static void main(String[] args) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver"); // Loaded drivers
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "toor");
-            System.out.prinntln("Connected successfully");
+            if (con != null) {
+                System.out.prinntln("Connected successfully");
+                Statement st = con.createStatement();
+                ResultSet rs = st.executeQuery("Use student");
+                rs = st.executeQuery("select * from studlist");
+                while (rs.next()) {
+                    System.out.println(rs.getInt("rollno") +
+                        rs.getString("firstname") + rs.getString("lastname") +
+                        rs.getString("brachn"));
+                }
+            }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
