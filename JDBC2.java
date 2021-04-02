361e9610cabc1cd418e763763ea7029d9741b787
Harsh Vardhan Guleria <harshvardhanguleria@gmail.com>
2021-04-02 10:37:06 +0530

    Modified

--- JDBC2.java
+++ JDBC2.java
@@ -1,24 +1,32 @@
 import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Scanner;
 
-public class JDBCSQL {
+public class JDBC2 {
+
+    private static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver"); // Loaded drivers
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "toor");
             if (con != null) {
                 System.out.println("Connected successfully");
-                Statement st = con.createStatement();
-                ResultSet rs = st.executeQuery("Use student");
-                rs = st.executeQuery("select * from studlist");
+                System.out.println("Enter the roll number to search for: ");
+                int rno = in.nextInt();
+
+                PreparedStatement pst = con.prepareStatement("select * from studlist where rollno = ?");
+                pst.setInt(1, rno);
+
+                ResultSet rs = pst.executeQuery();
                 while (rs.next()) {
                     System.out.println(rs.getInt("rollno") +
                         rs.getString("firstname") + rs.getString("lastname") +
                         rs.getString("brachn"));
                 }
-            }
+                }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
