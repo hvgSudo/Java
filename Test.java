b22be7031685c9a71af30e38393eb587e908ed4f
Harsh Vardhan Guleria <harshvardhanguleria@gmail.com>
2021-04-19 13:44:28 +0530

    Modified

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        Object a = 10;
+        int b = (Integer)a + 10;
+        System.out.println("a->"+ a);
+        System.out.println("b->"+ b);
+    }
+}
