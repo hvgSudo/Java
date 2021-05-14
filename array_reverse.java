efcd22a3b8e292596898c29b1578ec80d8e2c352
Harsh Vardhan Guleria <harshvardhanguleria@gmail.com>
2021-05-14 20:06:33 +0530

    Updated Database

--- array_reverse.java
+++ array_reverse.java
@@ -1,10 +1,37 @@
 
 import java.util.*;
+import java.io.*;
+
 public class array_reverse {
 
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    /*private static void fileInputOutput() {
+         // BufferedReader Class for Fast buffer Input
+        BufferedReader br = new BufferedReader(
+                               new FileReader("D:\\Codes\\Java\\inputJava.txt"));
+  
+        // PrintWriter class prints formatted representations
+        // of objects to a text-output stream.
+        PrintWriter pw=new PrintWriter(new
+                BufferedWriter(new FileWriter("D:\\Codes\\Java\\outputJava.txt")));
+  
+        // Your code goes Here
+  
+        pw.flush();
+    }*/
+
+    public static void main(String[] args) throws Exception {
+        // fileInputOutput();
+        BufferedReader br = new BufferedReader(
+                               new FileReader("D:\\Codes\\Java\\inputJava.txt"));
+  
+        // PrintWriter class prints formatted representations
+        // of objects to a text-output stream.
+        PrintWriter pw=new PrintWriter(new
+                BufferedWriter(new FileWriter("D:\\Codes\\Java\\outputJava.txt")));
+
+
         System.out.println("Enter size of the array: ");
         int n = Integer.parseInt(scanner.nextLine());
         System.out.println("Enter elements of the array: ");
@@ -15,6 +42,7 @@ public class array_reverse {
             System.out.print(a[i] +" ");
         }
         scanner.close();
+        pw.flush();
     }
 }
 
--- trying.java
+++ trying.java
@@ -1,45 +1,14 @@
-import java.util.Scanner;
-
 public class trying {
-
-        public static void main(String[] args) {
-                double n_1, sum = 0.0, count = 0.0;
-                String data;
-                Scanner s = new Scanner(System.in);
-                Calc Obj1 = new Calc();
-                data = s.nextLine();
-                while (true) {
-                        data = s.nextLine();
-                        Obj1.avg(data);
-                        if (data.equals("end"))
-                                break;
-                        else {
-                                n_1 = Double.parseDouble(data);
-                                sum = sum + n_1;
-                                count = count + 1;
-                        }
-                }
-                s.close();
-                Obj1.test(1);
-                Obj1.test(2);
-                Obj1.test(3);
-                Obj1.test(4);
+    public static void main(String[] args) {
+        try { 
+            int a[] = {1,2,3,4};
+            for (int i = 1; i <= 4; i++) {
+                System.out.println(i +"="+ a[i]);
+            }
+        } catch (Exception e) {
+            System.out.println("Error");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ArrayIndexoutofBound");
         }
-        
-}
-class Calc {
-        int b = 0;
-        double sum = 0, count = 0;
-        public void avg (String data) {
-                double num = Double.parseDouble(data);
-                sum = sum + num;
-                count = count + 1;
-                if (data.equals("end")) {
-                        System.out.println("Result: "+ (sum / count));
-                }
-        }
-        public void test (int a) {
-                b = b + 1;
-                System.out.println("b = "+ b);
-        }
+    }
 }
