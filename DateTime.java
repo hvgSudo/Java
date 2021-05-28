9084ec82db5e55abb4c2b9594ff8bac5a8934c7c
Harsh Vardhan Guleria <harshvardhanguleria@gmail.com>
2021-05-28 23:03:44 +0530

    Modified

--- DateTime.java
+++ DateTime.java
@@ -2,7 +2,7 @@ import java.time.LocalDateTime;
 
 public class DateTime {
 	public static void main(String[] args) {
-		System.out.println("Current Date: "+java.time.LocalDate.now());
+		System.out.println("\nCurrent Date: "+java.time.LocalDate.now());
 		System.out.println("Current Time: "+java.time.LocalTime.now());
 	}
 }
