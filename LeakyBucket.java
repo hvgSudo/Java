583d619a9d201aa0460dc7388b6fc250f4acb96c
kingston-machismo <harshvardhanguleria@gmail.com>
2021-11-14 13:20:29 +0530

    Modified

--- LeakyBucket.java
+++ LeakyBucket.java
@@ -1,46 +1,37 @@
+import java.io.PushbackReader;
+import java.util.Random;
+import java.util.Scanner;
+
 public class LeakyBucket {
-    public static void main (String[] args) {
-        int no_of_queries,storage,output_pkt_size;
-        int input_pkt_size,bucket_size,size_left;
-         
-         //initial packets in the bucket
-        storage=0;   
-         
-        //total no. of times bucket content is checked
-        no_of_queries=4;
-         
-        //total no. of packets that can
-        // be accommodated in the bucket
-        bucket_size=10;
-         
-        //no. of packets that enters the bucket at a time
-        input_pkt_size=4;
-         
-        //no. of packets that exits the bucket at a time
-        output_pkt_size=1;
-        for(int i=0;i<no_of_queries;i++)
-        {
-            size_left=bucket_size-storage; //space left
-             
-            if(input_pkt_size<=(size_left))        
-            {
-                storage+=input_pkt_size;
-                System.out.println("Buffer size= "+storage+
-                    " out of bucket size= "+bucket_size);
+    static void solution(int bucketSize, int packetSize, int output) {
+        if (packetSize > bucketSize)
+            System.out.println("Bucket overflow");
+        else {
+            while (packetSize > output) {
+                System.out.println(output +" bytes outputed");
+                packetSize = packetSize - output;
             }
-            else
-            {
-                System.out.println("Packet loss = "
-                            +(input_pkt_size-(size_left)));
-                             
-                     //full size      
-                storage=bucket_size;
-                 
-                System.out.println("Buffer size= "+storage+
-                            " out of bucket size= "+bucket_size);
-                 
+            if (packetSize > 0) {
+                System.out.println(packetSize +" bytes outputed");
             }
-            storage-=output_pkt_size;
+        }
+    }
+
+    public static void main(String[] args) {
+        int output, packetSize, bucketSize, n;
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+        System.out.print("Enter the bucket size(eg. 512): ");
+        bucketSize = sc.nextInt();
+        System.out.print("Enter the output rate: ");
+        output = sc.nextInt();
+        System.out.print("Enter the number of packets: ");
+        n = sc.nextInt();
+        sc.close();
+        for (int i = 1; i <= n; ++i) {
+            packetSize = rand.nextInt(1000);
+            System.out.println("Packet number: "+ i +"\tPacket size = "+ packetSize);
+            solution(bucketSize, packetSize, output); 
         }
     }
 }
